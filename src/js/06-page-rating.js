265619d80a3ed8a982795954af70b998acea897e
Hakim Cassimally <hakim.cassimally@couchbase.com>
2023-07-20 14:05:31 +0100

    rest-api-redocly

--- src/js/06-page-rating.js
+++ src/js/06-page-rating.js
@@ -1,18 +1,18 @@
 ;(function ($) {
   'use strict'
-  var dialogBox = document.getElementById('dialogBox')
+  // var dialogBox = document.getElementById('dialogBox')
   var helpYesBtn = document.getElementById('yesBtn')
   var helpNoBtn = document.getElementById('noBtn')
-  var skipBtnMsg = document.getElementById('skipBtnMsg')
+  // var skipBtnMsg = document.getElementById('skipBtnMsg')
   //var feedBackFormBox = document.getElementById('additionalFeedbackBox')
   //  var leaveAddtinalBox = document.getElementById('leaveAddtinalBox')
   // var skipLeaveBtn = document.getElementById('skipLeaveBtn')
-  var feedBackMsg = document.querySelector('.feed-back-msg')
-  var submitBtn = document.querySelector('.submit-btn')
+  // var feedBackMsg = document.querySelector('.feed-back-msg')
+  // var submitBtn = document.querySelector('.submit-btn')
   // var leaveYesBtn = document.querySelector('.yes-btn')
-  var feedbackInfoBtn = document.querySelector('.info-btn')
-  var feedbackModal = document.querySelector('.feedback-modal')
-  var closeModalPopup = document.querySelector('.close-popup')
+  // var feedbackInfoBtn = document.querySelector('.info-btn')
+  // var feedbackModal = document.querySelector('.feedback-modal')
+  // var closeModalPopup = document.querySelector('.close-popup')
   //var anyFeedbackBtn = document.querySelector('.any-feedback-btn')
   // for config
   if (helpYesBtn || helpNoBtn) {
@@ -30,27 +30,27 @@
       helpYesBtn.classList.remove('active')
       console.log(noBtnData, 27)
     })
-    skipBtnMsg.addEventListener('click', function (e) {
-      dialogBox.style.display = 'none'
-      feedBackMsg.value = ''
-    })
-
-    feedBackMsg.addEventListener('keyup', function (e) {
-      var textareaValue = this.value
-
-      if (textareaValue !== '') {
-        submitBtn.classList.remove('disabled')
-      } else {
-        submitBtn.classList.add('disabled')
-      }
-    })
-    feedbackInfoBtn.addEventListener('click', function (e) {
-      feedbackModal.classList.add('show')
-    })
-
-    closeModalPopup.addEventListener('click', function (e) {
-      feedbackModal.classList.remove('show')
-    })
+    //     skipBtnMsg.addEventListener('click', function (e) {
+    //       dialogBox.style.display = 'none'
+    //       feedBackMsg.value = ''
+    //     })
+    //
+    //     feedBackMsg.addEventListener('keyup', function (e) {
+    //       var textareaValue = this.value
+    //
+    //       if (textareaValue !== '') {
+    //         submitBtn.classList.remove('disabled')
+    //       } else {
+    //         submitBtn.classList.add('disabled')
+    //       }
+    //     })
+    //     feedbackInfoBtn.addEventListener('click', function (e) {
+    //       feedbackModal.classList.add('show')
+    //     })
+    //
+    //     closeModalPopup.addEventListener('click', function (e) {
+    //       feedbackModal.classList.remove('show')
+    //     })
   }
   // anyFeedbackBtn.addEventListener('click', function (e) {
   //   e.preventDefault()
@@ -69,7 +69,7 @@
     /*eslint-env es6*/
     'triggerFunction': function (showCollectorDialog) {
       //Requires that jQuery is available!
-      $('#myCustomTrigger').click(function (e) {
+      $('#feedbackTrigger').click(function (e) {
         e.preventDefault()
         showCollectorDialog()
       })
@@ -78,6 +78,7 @@
       'summary': `Feedback on ${document.title}`,
       'customfield_11580': window.location.href,
       'description': `\n\nBrowser environment: ${navigator.userAgent}`,
+      'components': `${document.getElementById('feedbackTrigger').dataset['jira-component-id']}`,
     },
   }
 
